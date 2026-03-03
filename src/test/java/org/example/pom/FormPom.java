9087ff2367422456c87058781a65a5944301e1de
Chiril Dones <doneskirill3@gmail.com>
2026-03-03 19:29:40 +0200

    init commit

---
+++ src/test/java/org/example/pom/FormPom.java
@@ -0,0 +1,87 @@
+package org.example.pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FormPom {
+
+    static public WebDriver driver;
+    static public JavascriptExecutor js;
+
+    @FindBy(xpath = "//*[text()='Forms']")
+    WebElement forms;
+
+    @FindBy(xpath = "//*[text()='Practice Form']")
+    WebElement practiceForm;
+
+    @FindBy(xpath = "//*[@id='firstName']")
+    WebElement firstName;
+
+    @FindBy(xpath = "//*[@id='lastName']")
+    WebElement lastName;
+
+    @FindBy(xpath = "//*[@id='userEmail']")
+    WebElement userEmail;
+
+    public FormPom(WebDriver driverParam) {
+        driver = driverParam;
+        js = (JavascriptExecutor) driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void setGender(String genderParam) {
+        WebElement gender = driver.findElement(By.xpath("//*[@id='genterWrapper']//label[text()='" + genderParam + "']"));
+        gender.click();
+    }
+
+    public void setEmail(String emailParam) {
+        userEmail.clear();
+        userEmail.sendKeys(emailParam);
+    }
+
+    public void setLastName(String lastNameParam) {
+        lastName.clear();
+        lastName.sendKeys(lastNameParam);
+    }
+
+    public void setFirstName(String firstNameParam) {
+        firstName.clear();
+        firstName.sendKeys(firstNameParam);
+    }
+
+    public void clickPracticeForm() {
+        practiceForm.click();
+    }
+
+    public void clickForms() {
+        forms.click();
+    }
+
+    public void pause(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void scrollToElement(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public void closeAdvert() {
+        try {
+            js.executeScript("var elem = document.evaluate(\"//*[@id='adplus-anchor']\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;" +
+                    "elem.parentNode.removeChild(elem);");
+        } catch (Exception ignored) {}
+        try {
+            js.executeScript("var elem = document.evaluate(\"//footer\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;" +
+                    "elem.parentNode.removeChild(elem);");
+        } catch (Exception ignored) {}
+    }
+}
---
+++ src/test/java/org/example/testng/FormTest.java
@@ -0,0 +1,47 @@
+package org.example.testng;
+
+import org.example.pom.FormPom;
+import org.example.utils.Driver;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FormTest {
+
+    static public WebDriver driver;
+    static public String URL = "https://demoqa.com/";
+    static public String FIRST_NAME = "Chiril";
+    static public String LAST_NAME = "Dones";
+    static public String EMAIL = "chirildones@gmail.com";
+    static public String GENDER = "Male";
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver = Driver.getAutoLocalDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void formTest() {
+        System.out.println("Start test");
+        driver.get(URL);
+        FormPom formPom = new FormPom(driver);
+        formPom.clickForms();
+        formPom.pause(1000);
+        formPom.clickPracticeForm();
+        formPom.closeAdvert();
+        formPom.setFirstName(FIRST_NAME);
+        formPom.setLastName(LAST_NAME);
+        formPom.setEmail(EMAIL);
+        formPom.setGender(GENDER);
+
+        System.out.println("Finish test");
+
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+}
